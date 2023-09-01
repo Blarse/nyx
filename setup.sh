294dfc0bcd36f875e325dffdd47806699970a9d2
Egor Ignatov <egori@altlinux.org>
2023-09-01 18:22:47 +0300

    setup.sh: setup llvm toolchain env

--- setup.sh
+++ setup.sh
@@ -2,11 +2,20 @@
 
 git submodule init
 
+export ALTWRAP_LLVM_VERSION=15.0
+export CC=clang
+export CXX=clang++
+export LD=ld.lld
+export AR=llvm-ar
+export NM=llvm-nm
+export RANLIB=llvm-ranlib
+
 # Build AFLplusplus
 git submodule update AFLplusplus 2>/dev/null
 cd AFLplusplus
 # fix afl_network_proxy build
 sed -i  '/^all:/i LDFLAGS += -ldl\n' ./utils/afl_network_proxy/GNUmakefile
+
 
 make NO_NYX=1 -j $(nproc) source-only
 cd -
